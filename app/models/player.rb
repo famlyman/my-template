0c573665ca6903a9e9ad6928bbd6003c01e85d2c
famlyman <burkecustomcomputers@gmail.com>
2023-09-13 19:24:08 -0400

    joinable teams

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,8 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :leagues, foreign_key: 'admin_id'
+  has_many :seasons, foreign_key: 'admin_id'
   has_and_belongs_to_many :teams
 
   enum role: [:player, :captain, :admin]
