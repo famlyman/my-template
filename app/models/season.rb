f8ca85711a47316e4d6639f14b4d411d8f37c6d1
Clay Dennis <burkecustomcomputers@gmail.com>
2023-09-18 16:24:02 -0400

    changed to teams from players

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,29 +5,29 @@ class Season < ApplicationRecord
 
 
     def generate_schedule
-        players = Player.all.to_a
-        num_players = players.length
+        teams = Team.all.to_a
+        num_teams = teams.length
     
-        # Ensure that the number of players is even for the round-robin algorithm
-        if num_players.odd?
-          players << nil
-          num_players += 1
+        # Ensure that the number of teams is even for the round-robin algorithm
+        if num_teams.odd?
+          teams << nil
+          num_teams += 1
         end
     
-        num_rounds = num_players - 1
+        num_rounds = num_teams - 1
     
         (1..num_rounds).each do |round|
           matches = []
     
-          players.shuffle.each_slice(2) do |pair|
+          teams.shuffle.each_slice(2) do |pair|
             matches << pair
           end
     
           matches.each do |pair|
-            Match.create(player1_id: pair[0]&.id, player2_id: pair[1]&.id, round: round, season_id: self.id)
+            Match.create(team_one_id: pair[0]&.id, team_two_id: pair[1]&.id, season_id: self.id)
           end
     
-          players.rotate!(-1) # Rotate players for the next round
+          teams.rotate!(-1) # Rotate teams for the next round
         end
     end
 
